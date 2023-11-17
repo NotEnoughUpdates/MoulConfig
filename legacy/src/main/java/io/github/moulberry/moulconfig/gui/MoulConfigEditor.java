b84dbb22dc6dae50773d0225775fc41a98a7dfd2
Linnea Gräf <nea@nea.moe>
2023-11-17 12:02:49 +0100

    Don't render expanded line for non rendering categories

--- legacy/src/main/java/io/github/moulberry/moulconfig/gui/MoulConfigEditor.java
+++ legacy/src/main/java/io/github/moulberry/moulconfig/gui/MoulConfigEditor.java
@@ -322,8 +322,9 @@ public class MoulConfigEditor<T extends Config> extends GuiElement {
             if (childCategories != null) {
                 var isExpanded = showSubcategories && (isSelected || childCategories.contains(getSelectedCategory()));
                 RenderUtils.drawOpenCloseTriangle(isExpanded, x + 24.5, y + 67 + catY, 6, 6);
-                if (isExpanded)
-                    drawVerticalLine(x + 27, y + catY + 76, y + catY + 76 + childCategories.size() * 15, 0xFF444444);
+                if (isExpanded) {
+                    drawVerticalLine(x + 27, y + catY + 76, y + catY + 76 + ((int) childCategories.stream().filter(currentConfigEditing::containsKey).count()) * 15, 0xFF444444);
+                }
             }
             catY += 15;
             if (catY > 0) {
