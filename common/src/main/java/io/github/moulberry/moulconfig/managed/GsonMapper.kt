4291b3553b3a6cdc5d8f4ac2cb6a2ca2508d256f
Linnea Gräf <nea@nea.moe>
2024-01-11 22:12:57 +0100

    Add legacy chromoa colour to default gson mapper

--- common/src/main/java/io/github/moulberry/moulconfig/managed/GsonMapper.kt
+++ common/src/main/java/io/github/moulberry/moulconfig/managed/GsonMapper.kt
@@ -1,11 +1,14 @@
 package io.github.moulberry.moulconfig.managed
 
 import com.google.gson.GsonBuilder
+import io.github.moulberry.moulconfig.ChromaColour
+import io.github.moulberry.moulconfig.LegacyStringChromaColourTypeAdapter
 import io.github.moulberry.moulconfig.observer.PropertyTypeAdapterFactory
 
 class GsonMapper<T>(val clazz: Class<T>) : DataMapper<T> {
     val gsonBuilder = GsonBuilder()
         .registerTypeAdapterFactory(PropertyTypeAdapterFactory())
+        .registerTypeAdapter(ChromaColour::class.java, LegacyStringChromaColourTypeAdapter(true))
     private val gson by lazy { gsonBuilder.create() }
     override fun serialize(value: T): String {
         return gson.toJson(value)
