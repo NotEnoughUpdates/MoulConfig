df8c9a90d2f70583b665e3e071497012f625f9b0
Linnea Gräf <nea@nea.moe>
2025-10-10 19:21:03 +0200

    build: load python in nix shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
+pkgs.mkShellNoCC {
   LD_LIBRARY_PATH=pkgs.lib.strings.makeLibraryPath [pkgs.libglvnd pkgs.zlib];
   shellHook = ''
     apply() {
       echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH" > .env
     }
   '';
+  packages = with pkgs; [(python3.withPackages (pp: [pp.pip]))];
 }
