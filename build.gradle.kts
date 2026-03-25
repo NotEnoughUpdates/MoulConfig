9aaf518220a3cc089b1b5ae3dc216bc7e49aa0c8
Linnea Gräf <nea@nea.moe>
2026-03-25 01:30:03 +0100

    build: fix dokka build

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,20 +16,17 @@ mkdocs {
 }
 
 val compileAllDocs = tasks.register("compileAllDocs", Copy::class) {
-	dependsOn(tasks.mkdocsBuild)
-	dependsOn(tasks.dokkaHtmlMultiModule)
-	destinationDir = layout.buildDirectory.dir("allDocs").get().asFile
-	from(tasks.mkdocsBuild)
-	from(tasks.dokkaHtmlMultiModule.get().outputDirectory) {
-		into("javadocs")
-	}
+    dependsOn(tasks.mkdocsBuild)
+    destinationDir = layout.buildDirectory.dir("allDocs").get().asFile
+    from(tasks.mkdocsBuild)
+    from(tasks.dokkaGeneratePublicationHtml.flatMap { it.outputDirectory }) {
+        into("javadocs")
+    }
 }
-
 val docJar = tasks.register("docJar", Zip::class) {
-	from(compileAllDocs)
-	archiveClassifier.set("docs")
+    from(compileAllDocs)
+    archiveClassifier.set("docs")
 }
-
 val docConfig = configurations.create("documentation")
 artifacts.add(docConfig.name, docJar)
 tasks.assemble { dependsOn(docJar) }
